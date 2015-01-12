4a687d25d8e8116be0a9994ffa31841e59554716
Alex <amurphy345@gmail.com>
2015-01-12 12:35:41 -0700

    Initial Commit

---
+++ Dog_Years/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dog_Years
+//
+//  Created by Alexander Murphy on 1/12/15.
+//  Copyright (c) 2015 Alexander Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
